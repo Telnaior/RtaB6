56a36db36f056a3f8671116207a8ee4e99c2dedc
Jerry <JerryRules@gmail.com>
2020-09-15 02:29:04 -0500

    Update CashForAll.java

--- src/tel/discord/rtab/events/CashForAll.java
+++ src/tel/discord/rtab/events/CashForAll.java
@@ -10,11 +10,11 @@ public class CashForAll implements EventSpace
 	{
 		//Give each living player from $100,000 to $250,000 in a random $50,000 increment.
 		int cashGiven = game.boardMultiplier * (100_000 + (50_000 * (int) (Math.random() * 4)));
-		for(Player nextPlayer : players)
+		for(Player nextPlayer : game.players)
 		{
 			if(nextPlayer.status == PlayerStatus.ALIVE)
 			{
-				game.players.get(player).addMoney(cashGiven);
+				game.players.get(player).addMoney(cashGiven, MoneyMultipliersToUse.BOOSTER_ONLY);
 			}
 		}
 		game.channel.sendMessage(String.format("It's **Cash For All**! All players remaining receive **$%,d**!",cashGiven)).queue();
