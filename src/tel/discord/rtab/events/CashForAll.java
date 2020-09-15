813eeba972e19e7b5dd8ea4446f6dc397a7a976a
Jerry <JerryRules@gmail.com>
2020-09-15 02:26:36 -0500

    Add Cash For All event

---
+++ src/tel/discord/rtab/events/CashForAll.java
@@ -0,0 +1,23 @@
+package tel.discord.rtab.events;
+
+import tel.discord.rtab.GameController;
+
+public class CashForAll implements EventSpace
+{
+
+	@Override
+	public void execute(GameController game, int player)
+	{
+		//Give each living player from $100,000 to $250,000 in a random $50,000 increment.
+		int cashGiven = game.boardMultiplier * (100_000 + (50_000 * (int) (Math.random() * 4)));
+		for(Player nextPlayer : players)
+		{
+			if(nextPlayer.status == PlayerStatus.ALIVE)
+			{
+				game.players.get(player).addMoney(cashGiven);
+			}
+		}
+		game.channel.sendMessage(String.format("It's **Cash For All**! All players remaining receive **$%,d**!",cashGiven)).queue();
+	}
+	
+}
