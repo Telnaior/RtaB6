9e7728d3d5ef298600c2446067feb01ee9561622
Telnaior <Telnaior@users.noreply.github.com>
2020-09-16 22:14:53 +1000

    Copy in remaining objects
    That should be the last of the old enums package moved into its proper place!

---
+++ src/tel/discord/rtab/games/objs/PokerHand.java
@@ -0,0 +1,27 @@
+package tel.discord.rtab.games.objs;
+
+public enum PokerHand {
+    NOTHING("Nothing"), // couldn't think of any other good name for a losing hand
+    ONE_PAIR("One Pair"),
+    TWO_PAIR("Two Pair"),
+    THREE_OF_A_KIND("Three of a Kind"),
+    STRAIGHT("Straight"), // A-2-3-4-5 and 10-J-Q-K-A both count, even though aces are normally high
+    FLUSH("Flush"),
+    FULL_HOUSE("Full House"),
+    FOUR_OF_A_KIND("Four of a Kind"),
+    STRAIGHT_FLUSH("Straight Flush"), // A-2-3-4-5 counts, but not 10-J-Q-K-A -- that's one of the royal hands
+    FIVE_OF_A_KIND("Five of a Kind"), // Only possible with deuces
+    WILD_ROYAL("Wild Royal Flush"),
+    FOUR_DEUCES("Four Deuces"),
+    NATURAL_ROYAL("Natural Royal Flush");
+    
+	private final String name;
+	
+	PokerHand (String name){
+		this.name = name;
+	}
+	
+    public String toString() {
+    	return name;
+    }
+}
