8da0b3b578434232aa987f3cd446210279b32497
StrangerCoug <strangercoug@hotmail.com>
2020-09-22 11:03:26 -0500

    Simplified method code and randomized game for each player

--- src/tel/discord/rtab/events/MinigamesForAll.java
+++ src/tel/discord/rtab/events/MinigamesForAll.java
@@ -1,8 +1,11 @@
 package tel.discord.rtab.events;
 
+import java.util.ArrayList;
+
 import tel.discord.rtab.GameController;
 import tel.discord.rtab.Player;
 import tel.discord.rtab.PlayerStatus;
+import tel.discord.rtab.board.Board;
 import tel.discord.rtab.board.Game;
 
 public class MinigamesForAll implements EventSpace
@@ -11,32 +14,25 @@ public class MinigamesForAll implements EventSpace
 	public void execute(GameController game, int player)
 	{
 		Game[] possibleGames = Game.values();
-		Game chosenGame;
-		
-		// Randomly select a minigame...
-		int[] cumulativeWeights = new int[possibleGames.length];
-		int totalWeight = 0;
-		for(int i=0; i<possibleGames.length; i++)
-		{
-			totalWeight += possibleGames[i].getWeight(0); // minigames don't care about player count
-			cumulativeWeights[i] = totalWeight;
-		}
-		double random = Math.random() * totalWeight;
-		int search=0;
-		while(cumulativeWeights[search] < random)
-			search++;
-		chosenGame = possibleGames[search];
-		
-		// ...and award it to everyone still in the game!
-		for(Player nextPlayer : game.players)
-		{
-			if(nextPlayer.status == PlayerStatus.ALIVE)
-				{
-					game.players.get(player).games.add(chosenGame);
-					game.players.get(player).games.sort(null);
-				}
-			}
+		ArrayList<Game> chosenGames = Board.generateSpaces(game.players.size(),1,Game.values());
+
 		game.channel.sendMessage("It's **Minigames For All**! All players remaining receive " +
-				"a copy of **" + chosenGame.getName() + "**!").queue();
+				"a copy of a randomly chosen minigame!").queue();
+		
+		
+		for(int i = 0; i < game.players.size(); i++)
+		{
+			Player nextPlayer = game.players.get(i);
+			Game chosenGame = chosenGames.get(i);
+			
+			if(nextPlayer.status == PlayerStatus.ALIVE)
+			{
+				game.players.get(player).games.add(chosenGame);
+				game.players.get(player).games.sort(null);
+				game.channel.sendMessage(nextPlayer.getSafeMention() +
+						" receives a copy of **" + chosenGame.getName() + "**!");
+			}
+		}
+		
 	}
 }
