e23b3984556bdf52329314048f4013d60b7f5f62
Telnaior <Telnaior@users.noreply.github.com>
2021-02-19 23:11:40 +1100

    Rework New Feature Achievement
    So now it won't always be two achievements in one

--- src/tel/discord/rtab/events/MinigamesForAll.java
+++ src/tel/discord/rtab/events/MinigamesForAll.java
@@ -36,7 +36,7 @@ public class MinigamesForAll implements EventSpace
 			game.players.get(player).games.sort(null);
 			game.players.get(player).minigameLock = true;
 			game.channel.sendMessage("Minigame Lock applied to "+game.players.get(player).getSafeMention()+".").queue();
-			Achievement.MINIGAME_FOR_ONE.check(game.players.get(player));
+			Achievement.NEW_FEATURE.check(game.players.get(player));
 		}
 		else
 		{
