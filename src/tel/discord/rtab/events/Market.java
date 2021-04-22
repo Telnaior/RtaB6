df8b07f9634d717d6089910262724c6a31a390b0
Telnaior <Telnaior@users.noreply.github.com>
2021-04-22 13:23:00 +1000

    Market bugfixes / additions
    more more more more more
    also added base multiplier because I would have *kicked* myself had I forgotten

--- src/tel/discord/rtab/events/Market.java
+++ src/tel/discord/rtab/events/Market.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import tel.discord.rtab.GameController;
 import tel.discord.rtab.MoneyMultipliersToUse;
 import tel.discord.rtab.Player;
+import tel.discord.rtab.PlayerStatus;
 import tel.discord.rtab.RaceToABillionBot;
 import tel.discord.rtab.board.Board;
 import tel.discord.rtab.board.Boost;
@@ -23,8 +24,7 @@ public class Market implements EventSpace
 {
 	static final int BUY_BOOST_PRICE = 10_000;
 	static final int SELL_BOOST_PRICE = 10_000;
-	static final int BUY_GAME_PRICE = 720_720; //divided by living players
-	static final int SELL_GAME_PRICE = 720_720; //divided by living players
+	static final int GAME_PRICE = 720_720; //divided by living players
 	static final int BUY_PEEK_PRICE = 1_000_000;
 	static final int SELL_PEEK_PRICE = 250_000;
 	static final int BUY_COMMAND_PRICE = 100_000;
@@ -58,7 +58,7 @@ public class Market implements EventSpace
 	}
 	private enum ChaosOption
 	{
-		BANKRUPT_CASH("$%,d","All bombs become Bankrupt")
+		BANKRUPT_CASH("$%,d", "All bombs become Bankrupt")
 		{
 			String getReward(GameController game, int player)
 			{
@@ -70,9 +70,97 @@ public class Market implements EventSpace
 			}
 			void applyResult(GameController game, int player)
 			{
-				game.channel.sendMessage("Very well! Here's your money, good luck keeping it!").queue();
+				game.channel.sendMessage("Chaos Option Selected. Here's your money, good luck keeping it!").queue();
 				game.players.get(player).addMoney(game.applyBaseMultiplier(1_000_000*game.playersAlive), MoneyMultipliersToUse.NOTHING);
 				game.gameboard.bankruptCurse();
+			}
+		},
+		SPLIT_AND_SHARE_ALL("Inflict Split and Share on All Opponents", "$%,d")
+		{
+			String getRisk(GameController game, int player)
+			{
+				return String.format(risk, (game.players.get(player).money/200)*game.playersAlive);
+			}
+			boolean checkCondition(GameController game, int player)
+			{
+				return game.players.get(player).money >= 100_000_000;
+			}
+			void applyResult(GameController game, int player)
+			{
+				game.channel.sendMessage("Chaos Option Selected. Good luck cashing in~").queue();
+				game.players.get(player).addMoney(-1*(game.players.get(player).money/200)*game.playersAlive, MoneyMultipliersToUse.NOTHING);
+				for(int i=0; i<game.players.size(); i++)
+					if(i != player && game.players.get(i).status == PlayerStatus.ALIVE)
+					{
+						game.players.get(i).splitAndShare = true;
+						game.channel.sendMessage("Split and Share applied to "+game.players.get(i).getSafeMention()+".").queue();
+					}
+			}
+		},
+		RISKY_JOKER("1 Joker", "Split and Share (if you lose)")
+		{
+			boolean checkCondition(GameController game, int player)
+			{
+				return game.players.get(player).jokers >= 0 && !game.players.get(player).splitAndShare;
+			}
+			void applyResult(GameController game, int player)
+			{
+				game.channel.sendMessage("Chaos Option Selected. Best of luck to you!").queue();
+				game.awardEvent(player, EventType.JOKER);
+				game.awardEvent(player, EventType.SPLIT_SHARE);
+			}
+		},
+		ELAVIA_BRIBE("$%,d", "Fold out of the round immediately")
+		{
+			String getReward(GameController game, int player)
+			{
+				return String.format(reward, game.applyBaseMultiplier(20_000*game.spacesLeft));
+			}
+			boolean checkCondition(GameController game, int player)
+			{
+				return game.spacesLeft > 0;
+			}
+			void applyResult(GameController game, int player)
+			{
+				game.channel.sendMessage("Chaos Option Selected. Thanks for playing!").queue();
+				game.players.get(player).addMoney(game.applyBaseMultiplier(20_000*game.spacesLeft), MoneyMultipliersToUse.NOTHING);
+				game.players.get(player).status = game.players.get(player).games.size()>0 ? PlayerStatus.FOLDED : PlayerStatus.OUT;
+				game.playersAlive--;
+				game.players.get(player).splitAndShare = false;
+			}
+		},
+		BUY_WINSTREAK("+%d.%d Streak", "$%,d")
+		{
+			int getPrice(int currentStreak)
+			{
+				if(currentStreak < 40)
+					return 5_000_000;
+				else if(currentStreak < 80)
+					return 15_000_000;
+				else if(currentStreak < 120)
+					return 50_000_000;
+				else
+					return 250_000_000; //This should never happen but just in case
+			}
+			String getReward(GameController game, int player)
+			{
+				int streakToGive = 40 - (game.players.get(player).winstreak % 40);
+				return String.format(reward, streakToGive/10, streakToGive%10);
+			}
+			String getRisk(GameController game, int player)
+			{
+				return String.format(risk, game.applyBaseMultiplier(getPrice(game.players.get(player).winstreak)));
+			}
+			boolean checkCondition(GameController game, int player)
+			{
+				return game.players.get(player).winstreak < 120 && game.players.size() <= 8;
+			}
+			void applyResult(GameController game, int player)
+			{
+				game.channel.sendMessage("Chaos Option Selected. Good luck in the bonus game!").queue();
+				game.players.get(player).addMoney(
+						game.applyBaseMultiplier(getPrice(game.players.get(player).winstreak)), MoneyMultipliersToUse.NOTHING);
+				game.players.get(player).addWinstreak(40 - (game.players.get(player).winstreak % 40));
 			}
 		};
 		
@@ -117,11 +205,11 @@ public class Market implements EventSpace
 		game.channel.sendMessage("It's the **RtaB Market**!").queue();
 		try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
 		//Decide on basic offerings
-		int boostBuyable = getCurrentPlayer().getRoundDelta() / BUY_BOOST_PRICE;
-		buyBoostAmount = Math.max(0, (int)((Math.random()*.9+.1)*boostBuyable));
+		int boostBuyable = getCurrentPlayer().getRoundDelta() / game.applyBaseMultiplier(BUY_BOOST_PRICE);
+		buyBoostAmount = Math.max(0, (int)((Math.random()*.8+.2)*boostBuyable));
 		int boostAvailable = getCurrentPlayer().booster / 2;
 		sellBoostAmount = boostAvailable > 50 ? (int)((Math.random()*.4+.1)*boostAvailable)+1 : 0;
-		int effectiveGamePrice = BUY_GAME_PRICE / game.playersAlive;
+		int effectiveGamePrice = game.applyBaseMultiplier(GAME_PRICE) / game.playersAlive;
 		if(getCurrentPlayer().getRoundDelta() >= effectiveGamePrice)
 			minigameOffered = Board.generateSpaces(1, game.players.size(), Game.values()).get(0);
 		//25% chance of chaos option
@@ -135,12 +223,14 @@ public class Market implements EventSpace
 		shopMenu.append("Available Wares:\n");
 		if(buyBoostAmount > 0)
 		{
-			shopMenu.append(String.format("BUY BOOST - +%d%% Boost (Cost: $%,d)\n", buyBoostAmount, buyBoostAmount*BUY_BOOST_PRICE));
+			shopMenu.append(String.format("BUY BOOST - +%d%% Boost (Cost: $%,d)\n",
+					buyBoostAmount, buyBoostAmount*game.applyBaseMultiplier(BUY_BOOST_PRICE)));
 			validOptions.add("BUY BOOST");
 		}
 		if(sellBoostAmount > 0)
 		{
-			shopMenu.append(String.format("SELL BOOST - $%,d (Cost: %d%% Boost)\n", sellBoostAmount*SELL_BOOST_PRICE, sellBoostAmount));
+			shopMenu.append(String.format("SELL BOOST - $%,d (Cost: %d%% Boost)\n",
+					sellBoostAmount*game.applyBaseMultiplier(SELL_BOOST_PRICE), sellBoostAmount));
 			validOptions.add("SELL BOOST");
 		}
 		if(minigameOffered != null)
@@ -153,24 +243,24 @@ public class Market implements EventSpace
 			shopMenu.append(String.format("SELL GAME - $%,d (Cost: Your Minigames)\n", getCurrentPlayer().games.size()*effectiveGamePrice));
 			validOptions.add("SELL GAME");
 		}
-		if(getCurrentPlayer().getRoundDelta() >= BUY_PEEK_PRICE)
+		if(getCurrentPlayer().getRoundDelta() >= game.applyBaseMultiplier(BUY_PEEK_PRICE))
 		{
-			shopMenu.append(String.format("BUY PEEK - 1 Peek (Cost: $%,d)\n", BUY_PEEK_PRICE));
+			shopMenu.append(String.format("BUY PEEK - 1 Peek (Cost: $%,d)\n", game.applyBaseMultiplier(BUY_PEEK_PRICE)));
 			validOptions.add("BUY PEEK");
 		}
 		if(getCurrentPlayer().peeks > 0)
 		{
-			shopMenu.append(String.format("SELL PEEK - $%,d (Cost: 1 Peek)\n", SELL_PEEK_PRICE));
+			shopMenu.append(String.format("SELL PEEK - $%,d (Cost: 1 Peek)\n", game.applyBaseMultiplier(SELL_PEEK_PRICE)));
 			validOptions.add("SELL PEEK");
 		}
 		if(true)
 		{
-			shopMenu.append(String.format("BUY COMMAND - Random Hidden Command (Cost: $%,d)\n", BUY_COMMAND_PRICE));
+			shopMenu.append(String.format("BUY COMMAND - Random Hidden Command (Cost: $%,d)\n", game.applyBaseMultiplier(BUY_COMMAND_PRICE)));
 			validOptions.add("BUY COMMAND");
 		}
 		if(true)
 		{
-			shopMenu.append(String.format("BUY INFO - List of Remaining Spaces (Cost: $%,d)\n", BUY_INFO_PRICE));
+			shopMenu.append(String.format("BUY INFO - List of Remaining Spaces (Cost: $%,d)\n", game.applyBaseMultiplier(BUY_INFO_PRICE)));
 			validOptions.add("BUY INFO");
 		}
 		if(chaosOption != null && chaosOption.checkCondition(game, player))
@@ -262,42 +352,43 @@ public class Market implements EventSpace
 		{
 		case "BUY BOOST":
 			game.channel.sendMessage("Boost bought!").queue();
-			getCurrentPlayer().addMoney(-1*buyBoostAmount*BUY_BOOST_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(-1*buyBoostAmount*game.applyBaseMultiplier(BUY_BOOST_PRICE), MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().addBooster(buyBoostAmount);
 			break;
 		case "SELL BOOST":
 			game.channel.sendMessage("Boost sold!").queue();
-			getCurrentPlayer().addMoney(sellBoostAmount*SELL_BOOST_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(sellBoostAmount*game.applyBaseMultiplier(SELL_BOOST_PRICE), MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().addBooster(-1*sellBoostAmount);
 			break;
 		case "BUY GAME":
 			game.channel.sendMessage("Minigame bought!").queue();
-			getCurrentPlayer().addMoney(-1*BUY_GAME_PRICE/game.playersAlive, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(-1*game.applyBaseMultiplier(GAME_PRICE)/game.playersAlive, MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().games.add(minigameOffered);
 			break;
 		case "SELL GAME":
 			game.channel.sendMessage("Minigames sold!").queue();
-			getCurrentPlayer().addMoney(getCurrentPlayer().games.size()*SELL_GAME_PRICE/game.playersAlive, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(
+					getCurrentPlayer().games.size()*game.applyBaseMultiplier(GAME_PRICE)/game.playersAlive, MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().games.clear();
 			break;
 		case "BUY PEEK":
 			game.channel.sendMessage("Peek bought!").queue();
-			getCurrentPlayer().addMoney(-1*BUY_PEEK_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(-1*game.applyBaseMultiplier(BUY_PEEK_PRICE), MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().peeks++;
 			break;
 		case "SELL PEEK":
 			game.channel.sendMessage("Peek sold!").queue();
-			getCurrentPlayer().addMoney(SELL_PEEK_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(game.applyBaseMultiplier(SELL_PEEK_PRICE), MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().peeks--;
 			break;
 		case "BUY COMMAND":
 			game.channel.sendMessage("Command bought!").queue();
-			getCurrentPlayer().addMoney(-1*BUY_COMMAND_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(-1*game.applyBaseMultiplier(BUY_COMMAND_PRICE), MoneyMultipliersToUse.NOTHING);
 			getCurrentPlayer().awardHiddenCommand();
 			break;
 		case "BUY INFO":
 			game.channel.sendMessage("Information coming your way!").queue();
-			getCurrentPlayer().addMoney(-1*BUY_INFO_PRICE, MoneyMultipliersToUse.NOTHING);
+			getCurrentPlayer().addMoney(-1*game.applyBaseMultiplier(BUY_INFO_PRICE), MoneyMultipliersToUse.NOTHING);
 			if(!getCurrentPlayer().isBot) //A bot should never get here and we don't want to try sending a message to it if it somehow does
 			{
 				//Prepare the 2D list
@@ -360,7 +451,7 @@ public class Market implements EventSpace
 			switch(shopWeapon)
 			{
 			case ROCK:
-				game.channel.sendMessage("...and find them carrying a rock of their own.").queue();
+				game.channel.sendMessage("...and find them carrying a rock of their own. A tie?!").queue();
 				try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
 				switch(backupWeapon)
 				{
@@ -398,7 +489,7 @@ public class Market implements EventSpace
 				robberySuccess();
 				break;
 			case PAPER:
-				game.channel.sendMessage("...and find them carrying some of their own.").queue();
+				game.channel.sendMessage("...and find them carrying some paper of their own. A tie?!").queue();
 				try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
 				switch(backupWeapon)
 				{
@@ -433,7 +524,7 @@ public class Market implements EventSpace
 				robberySuccess();
 				break;
 			case SCISSORS:
-				game.channel.sendMessage("...and find them carrying some scissors of their own.").queue();
+				game.channel.sendMessage("...and find them carrying some scissors of their own. A tie?!").queue();
 				try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
 				switch(backupWeapon)
 				{
@@ -467,6 +558,8 @@ public class Market implements EventSpace
 		game.awardBoost(player, Boost.P150);
 		try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
 		game.awardCash(player, Cash.P1000K);
+		if(getCurrentPlayer().hiddenCommand == null)
+			getCurrentPlayer().awardHiddenCommand();
 		try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); } //mini-suspense lol
 		game.awardEvent(player, EventType.PEEK_REPLENISH);
 	}
