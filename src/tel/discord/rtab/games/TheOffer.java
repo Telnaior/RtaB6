09f471cd07700b8a5fa79d1fad7ecf8cf5b3a0d9
Telnaior <Telnaior@users.noreply.github.com>
2021-12-08 13:34:10 +1100

    Revamp The Offer
    who needs one offer when you can have three :)

--- src/tel/discord/rtab/games/TheOffer.java
+++ src/tel/discord/rtab/games/TheOffer.java
@@ -4,14 +4,17 @@ import java.util.LinkedList;
 
 public class TheOffer extends MiniGameWrapper
 {
-	static final String NAME = "The Offer";
-	static final String SHORT_NAME = "Offer";
+	static final String NAME = "Three Offers";
+	static final String SHORT_NAME = "Offers";
 	static final boolean BONUS = false;
-	double chanceToBomb; 
-	int offer;
-	int seconds;
-	boolean alive; //Player still alive?
-	boolean accept; //Accepting the Offer
+	int chanceToBomb = 0; // Expressed as percentage
+	int round = 0;
+	int total = 50_000; // Current bank
+	int[] offer = new int[3];
+	int[] ticks = new int[3];
+	boolean alive = true; //Player still alive?
+	
+	enum OfferLabel { LOW, MEDIUM, HIGH };
 	
 	/**
 	 * Initialises the variables used in the minigame and prints the starting messages.
@@ -19,21 +22,22 @@ public class TheOffer extends MiniGameWrapper
 	@Override
 	void startGame()
 	{
-		offer = 1000 * (int)(Math.random()*51+50); // First Offer starts between 50,000 and 100,000
-		chanceToBomb = offer/10000;  // Start chance to Bomb 5-10% based on first offer
-		seconds = 1;
-		alive = true; 
-		accept = false;
-
+		total = applyBaseMultiplier(total);
 		LinkedList<String> output = new LinkedList<>();
 		//Give instructions
-		output.add("In The Offer, you can enter a room with a live bomb.");
-		output.add("Every room you survive will earn you more money," +
-				"but the chance of the bomb exploding will also increase significantly.");
-		output.add("Every room will also increase the amount of chances to explode!");
-		output.add("If the bomb explodes, you lose everything."); //~Duh
+		output.add("In Three Offers, you can enter a room with a live bomb.");
+		output.add("In each room, there will be three offers for how long to spend with the bomb. "
+				+ "You'll earn more money for taking on a greater risk... *if* you survive.");
+		output.add("Each room will feature a more volatile bomb, "
+				+ "and you'll lose everything if you're still standing there when it explodes."); //~Duh
+		output.add(String.format("Choose your offers wisely, and good luck! You start with **$%,d**.",total));
+		if(enhanced)
+			output.add("ENHANCE BONUS: The Banker sees the explode chance as 5% higher than it really is.");
 		sendSkippableMessages(output);
-		sendMessage(makeOffer(offer, seconds, chanceToBomb));
+		output.clear();
+		output.add(makeOffers());
+		output.add("Type the offer you want to accept, or STOP to leave with your current total.");
+		sendMessages(output);
 		getInput();
 	}
 
@@ -47,7 +51,23 @@ public class TheOffer extends MiniGameWrapper
 		LinkedList<String> output = new LinkedList<>();
 		String choice = pick.toUpperCase();
 		choice = choice.replaceAll("\\s","");
-		if(choice.equals("REFUSE") || choice.equals("NODEAL") || choice.equals("DARE"))
+		if("STOP".startsWith(choice) || "QUIT".startsWith(choice))
+		{
+			output.add("You took the money!");
+			awardMoneyWon(applyBaseMultiplier(total));
+			return;
+		}
+		int seconds = 0;
+		for(OfferLabel next : OfferLabel.values())
+		{
+			if(next.toString().startsWith(choice))
+			{
+				total = offer[next.ordinal()];
+				seconds = ticks[next.ordinal()];
+				break;
+			}
+		}
+		if(seconds > 0)
 		{
 			output.add("The Bomb goes live!");
 			output.add("...");
@@ -56,88 +76,96 @@ public class TheOffer extends MiniGameWrapper
 			{
 				if (chanceToBomb > Math.random()*100)
 				{
-					output.add("**BOOM**");
+					output.add(String.format("Tick %d... **BOOM**",i+1));
 					alive = false;
 					break;
 				}
 				else
-					output.add(String.format("You survived Tick %d!",i+1));
+					output.add(String.format("Tick %d... _\\*tick*_",i+1));
 			}
+			sendMessages(output);
+			output.clear();
 			//If still alive, let's run it
 			if(alive)
 			{
-				double increment = Math.random()*0.5;
-				offer += (int)(offer * (1 + increment));
-				offer -= offer%100;
-				seconds++;
-				chanceToBomb += 5 + (increment*10);
-				output.add(makeOffer(offer, seconds, chanceToBomb));
+				output.add("You survived!");
+				output.add(makeOffers());
+				sendMessages(output);
+			}
+			else
+			{
+				awardMoneyWon(0);
 			}
 		}
-		else if(choice.equals("ACCEPT") || choice.equals("DEAL") || choice.equals("TAKE"))
-		{
-			accept = true;
-			output.add("You took the money!");
-		}
 		//If it's neither of those it's just some random string we can safely ignore
-		sendMessages(output);
-		if(isGameOver())
-			awardMoneyWon(getMoneyWon());
-		else
-			getInput();
+		getInput();
 	}
 
 	/**
-	* @param offer The amount that gets offered to the Player
-	* @param times The amount of times the Bomb will Tick
-	* @param bomb The Chance of the Bomb going Boom per Tick
-	* @return Will Return a nice looking output with all Infos
+	* @return Will prepare the next offers and return the display
 	**/
-	private String makeOffer(int offer, int times, double bomb)
+	private String makeOffers()
 	{
+		round++;
+		chanceToBomb += 5;
+		if(enhanced) // Enhance bonus makes the banker see the chance as higher than reality
+			chanceToBomb += 5;
+		//Figure out how many ticks we want each offer to be
+		double tickMod = (100.0-chanceToBomb)/100;
+		ticks[0] = 1; //Low offer always 1 tick
+		ticks[1] = (int)((Math.random()*(4*tickMod))+2); //Med offer 2-5 ticks at first, with upper bound reducing over time
+		ticks[2] = (int)((Math.random()*(4*tickMod))+ticks[1]+1); // High offer 1-4 above the med offer, also reducing over time...
+		if(Math.random() < (tickMod/10)) //But occasionally boost it to a super offer
+			ticks[2] += (int)(5*tickMod); // Final high offer can be anything from 3-13 ticks, though the chance drops quickly
+		//Now calculate the prices for each offer
+		for(int i=0; i<3; i++)
+		{
+			offer[i] = (int)(total * Math.pow(1+(chanceToBomb/100.0), ticks[i]*2));
+			offer[i] -= offer[i] % applyBaseMultiplier(1000);
+		}
+		if(enhanced)
+			chanceToBomb -= 5;
+		//Then print it all out!
 		StringBuilder output = new StringBuilder();
 		output.append("```\n");
-		output.append("  The Offer  \n\n");
-		output.append("Next Room:\n");
-		output.append("Bomb: " + String.format ("%.2f%%\n", bomb));
-		output.append("Ticks: " + String.format("%,d Times\n\n", times));
-		output.append("Current Money: " + String.format("$%,d\n\n", applyBaseMultiplier(offer)));
-
-		output.append(" 'Take' the Money  or  'Dare' the Bomb \n");
+		output.append(String.format("ROOM %d\n",round));
+		output.append(String.format("Bomb Explode Chance: %d%% per tick\n", chanceToBomb));
+		output.append(String.format("STOP: Leave with $%,d\n\n", total));
+		output.append("  Three Offers  \n");
+		for(OfferLabel next : OfferLabel.values())
+		{
+			output.append(String.format("%s: Survive %d ticks for $%,d\n", next.toString(), ticks[next.ordinal()], offer[next.ordinal()]));
+		}
 		output.append("```");
 		return output.toString();
 	}
-
-	/**
-	 * Returns true if the minigame has ended
-	 */
-	private boolean isGameOver()
-	{
-		return accept || !alive;
-	}
-
 
 	/**
 	 * Returns an int containing the player's winnings, pre-booster.
 	 */
 	private int getMoneyWon()
 	{
-		return alive ? applyBaseMultiplier(offer) : 0;
+		return alive ? applyBaseMultiplier(total) : 0;
 	}
 	
 	@Override
 	String getBotPick()
 	{
-		//Do a "trial run", quit if it fails
-		for(int i=0; i<=seconds; i++)
+		//Do a "trial run", take the highest offer it says we'll survive
+		for(int i=0; i<ticks[2]; i++)
 		{
 			if (chanceToBomb > Math.random()*100)
 			{
-				return "ACCEPT";
+				if(i < ticks[0])
+					return "STOP";
+				else if(i < ticks[1])
+					return "LOW";
+				else
+					return "MEDIUM";
 			}
 		}
-		//Trial run says we'll survive, so play on
-		return "REFUSE";
+		//Trial run says we'll survive everything, so take the high offer
+		return "HIGH";
 	}
 	
 	@Override
@@ -153,19 +181,8 @@ public class TheOffer extends MiniGameWrapper
 		awardMoneyWon(getMoneyWon());
 	}
 
-	@Override
-	public String getName()
-	{
-		return NAME;
-	}
-	@Override
-	public String getShortName()
-	{
-		return SHORT_NAME;
-	}
-	@Override
-	public boolean isBonus()
-	{
-		return BONUS;
-	}
+	@Override public String getName() { return NAME; }
+	@Override public String getShortName() { return SHORT_NAME; }
+	@Override public boolean isBonus() { return BONUS; }
+	@Override public String getEnhanceText() { return "The Banker will treat the explode chance as 5% higher than it really is."; }
 }
