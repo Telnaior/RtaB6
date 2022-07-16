caf7739b9090b97e9bb2f3429361728d3c533d1c
Jeffrey Hope <strangercoug@hotmail.com>
2022-07-16 10:54:57 -0500

    Remove unused import

--- src/tel/discord/rtab/commands/AnnuitiesCommand.java
+++ src/tel/discord/rtab/commands/AnnuitiesCommand.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 
