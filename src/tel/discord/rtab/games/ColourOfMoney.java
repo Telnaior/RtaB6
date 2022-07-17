c4901fd1d94317adcc7283b65659aaa9deff5027
Telnaior <Telnaior@users.noreply.github.com>
2022-07-17 13:47:36 +1000

    adjust valueOf() comment
    turns out the code inspector got this right so I guess I'll fix the comment

--- src/tel/discord/rtab/games/ColourOfMoney.java
+++ src/tel/discord/rtab/games/ColourOfMoney.java
@@ -201,7 +201,7 @@ public class ColourOfMoney extends PvPMiniGameWrapper
 			else
 				output.add(String.format("Withdrawing $%,d from the %s bank. It contains...", withdrawalAmount, colours.get(chosenBank)));
 			pickedSpaces[chosenBank] = true;
-			remainingValues.remove(values.get(chosenBank)); //valueOf trickery needed to avoid value being read as an index
+			remainingValues.remove(values.get(chosenBank)); //values.get() returns Integer so remove() sees it as an object properly
 			incrementTurnCount();
 			//Suspense if you're playing it risky
 			if(withdrawalAmount > adjustedBase*BOARD_SIZE/2)
