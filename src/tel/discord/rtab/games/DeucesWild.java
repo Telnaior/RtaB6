4c7b22be75f7d7928887c51d70e736d3ade13d8b
Jeffrey Hope <strangercoug@hotmail.com>
2022-07-18 10:24:20 -0500

    Use Arrays.fill()

--- src/tel/discord/rtab/games/DeucesWild.java
+++ src/tel/discord/rtab/games/DeucesWild.java
@@ -81,8 +81,7 @@ public class DeucesWild extends MiniGameWrapper
 					redrawUsed = false;
 					output.add("Stopping would prevent you from winning anything. Hold at least one card, then type 'DEAL'.");
 				} else {
-					for (int i = 0; i < cardsHeld.length; i++)
-						cardsHeld[i] = true;
+					Arrays.fill(cardsHeld, true);
 					redrawUsed = true;
 					output.add(generateBoard(true));
 				}
