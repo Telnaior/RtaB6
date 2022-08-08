e13e243654e9a0c9d27c8497fdf11a93f6974e30
Telnaior <Telnaior@users.noreply.github.com>
2022-08-08 12:28:23 +1000

    TTB Major Prize just *slightly* less
    not entirely sure what I was thinking with 5m but we'll see how it goes

--- src/tel/discord/rtab/games/TicTacBomb.java
+++ src/tel/discord/rtab/games/TicTacBomb.java
@@ -19,7 +19,7 @@ public class TicTacBomb extends PvPMiniGameWrapper
 	private static final boolean BONUS = false;
 	private static final int PRIZE_PER_SAFE_SPACE = 50_000;
 	private static final int PRIZE_FOR_MINOR_WIN = 1_000_000;
-	private static final int PRIZE_FOR_MAJOR_WIN = 5_000_000;
+	private static final int PRIZE_FOR_MAJOR_WIN = 2_500_000;
 	private static final int[][] LINES = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 	private final int[] spaces = new int[9];
 	private int playerBomb = -1;
