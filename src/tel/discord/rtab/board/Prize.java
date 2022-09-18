0aa61e6fd4af70d355eced296df7eb5e57441fa1
JerryEris <jerryrules@gmail.com>
2022-09-18 17:39:00 -0500

    Prize Update 1a
    Ignores .bak files, arranges prizes from low to high value

--- src/tel/discord/rtab/board/Prize.java
+++ src/tel/discord/rtab/board/Prize.java
@@ -16,49 +16,50 @@ public enum Prize {
 	S10TROPHY(   28000,"a replica of LA_Bud's Season 10 trophy"), 
 	
 	//(Ir)Regular prizes
-	DB1	 		(   22805,"a DesertBuck"), //Desert Bus for Hope
-	GOVERNOR	(   26000,"the Governor's favourite"), //Deal or No Deal UK
-	PI			(   31416,"a fresh pi"), //Maths
-	ECONSTANT	(   27183,"some e"), //Maths
-	SIXTEENBIT	(   65536,"a 16-bit trophy"), //2^16
-	SECONDS		(   86400,"a dollar a second"), //For a day
-	VOWEL		(     250,"a vowel"), //Wheel of Fortune
-	QUESTION	(   64000,"the famous question"), //The $64,000 Question
-	BIGJON		(    1906,"the BigJon special"), //BigJon
-	WEIRDAL		(   27000,"Weird Al's accordion"), //Weird Al
-	ROCKEFELLER	(    1273,"a trip down Rockefeller Street"), //Eurovision song that became an osu! meme
-	EWW			(     144,"something gross"), //Twelve dozen = a gross
-	HUNDREDG	(  100000,"a 100 Grand bar"), //Candy
-	HUNTER		(   22475,"Superportal codes"), //Spyro 2
-	FEUD		(   20000,"Fast Money"), //Family Feud
+	JOKERSIKE	(       1,"a fake Joker"), //RtaB meme
+	ZONK		(     123,"a Zonk"), //Let's Make a Deal
 	STCHARLES	(     140,"a trip to St. Charles Place"), //Monopoly
-	GOTOGO		(     200,"Advance to GO"), //Monopoly
-	BOARDWALK	(    2000,"rent at the Boardwalk Hotel"), //Monopoly (Three cheap prizes, who'd have thought)
+	EWW			(     144,"something gross"), //Twelve dozen = a gross
 	SNOOKER		(     147,"a snooker table"), //Maximum Break in Snooker
 	DARTBOARD	(     180,"a dartboard"), //3x Triple 20
-	GRANDPIANO	(   88000,"a grand piano"), //Number of keys on a piano
-	NORMALCD	(   54321,"a Normal Countdown"), //Not a Final Countdown
-	ZONK		(     123,"a Zonk"), //Let's Make a Deal
+	SMALLTHINGS	(     182,"All the Small Things"), //Song by Blink-182
+	GOTOGO		(     200,"Advance to GO"), //Monopoly
+	VOWEL		(     250,"a vowel"), //Wheel of Fortune
+	TWEET		(     280,"a Tweet"); //280 characters in a Tweet
+	BOWLING		(     300,"a bowling ball"); //Maximum score in Bowling
+	UNIFORCES	(     333,"the Triforce!!! Wait, no, that's just three Monoforces"), //Try again
+	FLOKATI		(     350,"a Flokati Rug"), //Press Your Luck joke prize
+	SLOTJACKPOT	(     777,"the JACKPOT... on a slot machine"); //7-7-7 is a traditionally high-value line on a slot machine
+	PEANUTS		(    1000,"a bucket of peanuts"), //Blaseball
+	ROCKEFELLER	(    1273,"a trip down Rockefeller Street"), //Eurovision song that became an osu! meme
+	BIGJON		(    1906,"the BigJon special"), //BigJon
+	BOARDWALK	(    2000,"rent at the Boardwalk Hotel"), //Monopoly (Three cheap prizes, who'd have thought)
+	ATARI		(    2600,"a wood-paneled Atari system"); //The Atari VCS, also known as the Atari 2600
 	FREELIVES	(    9900,"99 free lives! ...in Mario"), //100 coins for an extra life
 	BUTTSPIE	(   18000,"a slice of Butterscotch Pie"), //Undertale
-	DISCORD		(   70013,"a Discord lamp"), //My Little Pony
-	PYRAMID		(  100000,"The Pyramid"), //The $100,000 Pyramid
+	FEUD		(   20000,"Fast Money"), //Family Feud
 	BIFORCE		(   22222,"The Biforce"), //Still haven't found the Triforce, huh? Better keep looking!
-	JOKERSIKE	(       1,"a fake Joker"), //RtaB meme
-	SMALLTHINGS	(     182,"All the Small Things"), //Song by Blink-182
-	LARSON		(  110237,"The Michael Larson special"), //Winnings of the man who exploited Press Your Luck
-	FLOKATI		(     350,"a Flokati Rug"), //Press Your Luck joke prize
-	POWERPELLET	(   76500,"a Power Pellet"), //Namco, who created Pacman, is 765 in Goroawase wordplay
-	SPREAD		(   57300,"the Spread Shot"), //Konami, who created Contra, is 573 in Goroawase wordplay
-	PEANUTS		(    1000,"a bucket of peanuts"), //Blaseball
-	ROADSTER	(   50000,"a sleek 1999 roadster"), //Smash TV
-	BOWLING		(     300,"a bowling ball"); //Maximum score in Bowling
+	HUNTER		(   22475,"Superportal codes"), //Spyro 2
+	DB1	 		(   22805,"a DesertBuck"), //Desert Bus for Hope
+	GOVERNOR	(   26000,"the Governor's favourite"), //Deal or No Deal UK
+	WEIRDAL		(   27000,"Weird Al's accordion"), //Weird Al
+	ECONSTANT	(   27183,"some e"), //Maths
+	PI			(   31416,"a fresh pi"), //Maths
 	GOLDWII		(   36000,"a gold-plated Nintendo Wii"); //Once owned by Queen Elizabeth II
-	SLOTJACKPOT	(     777,"the JACKPOT... on a slot machine"); //7-7-7 is a traditionally high-value line on a slot machine
-	ATARI		(    2600,"a wood-paneled Atari system"); //The Atari VCS, also known as the Atari 2600
-	PIZZABOX	(  458000,"an infinitely regerating pizza box"); //SCP-458
-	TWEET		(     280,"a Tweet"); //280 characters in a Tweet
+	ROADSTER	(   50000,"a sleek 1999 roadster"), //Smash TV
+	NORMALCD	(   54321,"a Normal Countdown"), //Not a Final Countdown
+	SPREAD		(   57300,"the Spread Shot"), //Konami, who created Contra, is 573 in Goroawase wordplay
+	QUESTION	(   64000,"the famous question"), //The $64,000 Question
+	SIXTEENBIT	(   65536,"a 16-bit trophy"), //2^16
+	DISCORD		(   70013,"a Discord lamp"), //My Little Pony
+	POWERPELLET	(   76500,"a Power Pellet"), //Namco, who created Pacman, is 765 in Goroawase wordplay
+	SECONDS		(   86400,"a dollar a second"), //For a day
+	GRANDPIANO	(   88000,"a grand piano"), //Number of keys on a piano
+	HUNDREDG	(  100000,"a 100 Grand bar"), //Candy
+	PYRAMID		(  100000,"The Pyramid"), //The $100,000 Pyramid
+	LARSON		(  110237,"The Michael Larson special"), //Winnings of the man who exploited Press Your Luck
 	VINTTWEET	(  140000,"a vintage Tweet"); //There used to be 140 characters in a Tweet
+	PIZZABOX	(  458000,"an infinitely regerating pizza box"); //SCP-458
     
     private final int prizeValue;
     private final String prizeName;
