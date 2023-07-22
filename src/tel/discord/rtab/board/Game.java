7fb4306bdb1c991b20d4347e6b2a44e322d736f7
Telnaior <Telnaior@users.noreply.github.com>
2023-07-22 15:13:15 +1000

    Minigame Rotation
    15 in the pool with the prior addition of Stardust

--- src/tel/discord/rtab/board/Game.java
+++ src/tel/discord/rtab/board/Game.java
@@ -5,21 +5,19 @@ import tel.discord.rtab.games.*;
 public enum Game implements WeightedSpace
 {
 	//Minigame Pool
-	//Seasonal event is a minigame?? Guess it's a season of minigames
-	COMMISSIONER(2)		{ public MiniGame getGame() { return new TheCommissioner(); } },
 	//Minigames awarding weird things appear first
 	OVERFLOW(1)			{ public MiniGame getGame() { return new Overflow(); } },		//Author: JerryEris
 	//PvP games next so the opponent doesn't fall asleep waiting for them
-	TIC_TAC_BOMB(1)		{ public MiniGame getGame() { return new TicTacBomb(); } },		//Author: Atia
-	COLOUR_OF_MONEY(1)	{ public MiniGame getGame() { return new ColourOfMoney(); } },	//Author: Atia
+	COLOUR_OF_MONEY(1)	{ public MiniGame getGame() { return new ColourOfMoney(); } },	//Author: Telna
 	//Regular cash games
-	DEAL_OR_NO_DEAL(1)	{ public MiniGame getGame() { return new DealOrNoDeal(); } },	//Author: Atia
-	SAFE_CRACKER(1)		{ public MiniGame getGame() { return new SafeCracker(); } },	//Author: Atia
-	STRIKE_IT_RICH(1)	{ public MiniGame getGame() { return new StrikeItRich(); } },	//Author: Atia
-	TRIPLE_PLAY(1)		{ public MiniGame getGame() { return new TriplePlay(); } },		//Author: Atia
+	DEAL_OR_NO_DEAL(1)	{ public MiniGame getGame() { return new DealOrNoDeal(); } },	//Author: Telna
+	MATH_TIME(1)		{ public MiniGame getGame() { return new MathTime(); } },		//Author: Telna
+	SAFE_CRACKER(1)		{ public MiniGame getGame() { return new SafeCracker(); } },	//Author: Telna
+	STRIKE_IT_RICH(1)	{ public MiniGame getGame() { return new StrikeItRich(); } },	//Author: Telna
+	TRIPLE_PLAY(1)		{ public MiniGame getGame() { return new TriplePlay(); } },		//Author: Telna
 	CLOSE_SHAVE(1)		{ public MiniGame getGame() { return new CloseShave(); } },		//Author: JerryEris
-	DOUBLE_ZEROES(1)	{ public MiniGame getGame() { return new DoubleZeroes(); } },	//Author: JerryEris
 	STARDUST(1)			{ public MiniGame getGame() { return new Stardust(); } },		//Author: NicoHolic777
+	BOMB_ROULETTE(1)	{ public MiniGame getGame() { return new BombRoulette(); } },	//Author: StrangerCoug
 	MONEY_CARDS(1)		{ public MiniGame getGame() { return new MoneyCards(); } },		//Author: StrangerCoug
 	PUNCH_A_BUNCH(1)	{ public MiniGame getGame() { return new PunchABunch(); } },    //Author: StrangerCoug
 	SHUT_THE_BOX(1)		{ public MiniGame getGame() { return new ShutTheBox(); } },		//Author: StrangerCoug
@@ -27,23 +25,25 @@ public enum Game implements WeightedSpace
 	BUMPER_GRAB(1)		{ public MiniGame getGame() { return new BumperGrab(); } },		//Author: Tara
 	
 	//Games rotated out
+	TIC_TAC_BOMB(0)		{ public MiniGame getGame() { return new TicTacBomb(); } },		//Author: Telna
 	TESTGAME(0)			{ public MiniGame getGame() { return new TestGame(); } },		//Author: The Triforce
-	BOOSTER_SMASH(0)	{ public MiniGame getGame() { return new BoosterSmash(); } },	//Author: Atia
-	FTROTS(0)			{ public MiniGame getGame() { return new FTROTS(); } },			//Author: Atia
-	GAMBLE(0)			{ public MiniGame getGame() { return new Gamble(); } },			//Author: Atia
-	MATH_TIME(0)		{ public MiniGame getGame() { return new MathTime(); } },		//Author: Atia
-	OPTIMISE(0)			{ public MiniGame getGame() { return new Optimise(); } },		//Author: Atia
+	BOOSTER_SMASH(0)	{ public MiniGame getGame() { return new BoosterSmash(); } },	//Author: Telna
+	FTROTS(0)			{ public MiniGame getGame() { return new FTROTS(); } },			//Author: Telna
+	GAMBLE(0)			{ public MiniGame getGame() { return new Gamble(); } },			//Author: Telna
+	OPTIMISE(0)			{ public MiniGame getGame() { return new Optimise(); } },		//Author: Telna
 	COINFLIP(0)			{ public MiniGame getGame() { return new CoinFlip(); } },		//Author: Amy
 	MINEFIELD_MULTI(0)	{ public MiniGame getGame() { return new MinefieldMulti(); } },	//Author: Amy
 	THE_OFFER(0)		{ public MiniGame getGame() { return new TheOffer(); } },		//Author: Amy
 	CALL_YOUR_SHOT(0)	{ public MiniGame getGame() { return new CallYourShot(); } },	//Author: JerryEris
 	DOUBLE_TROUBLE(0)	{ public MiniGame getGame() { return new DoubleTrouble(); } },	//Author: JerryEris
+	DOUBLE_ZEROES(0)	{ public MiniGame getGame() { return new DoubleZeroes(); } },	//Author: JerryEris
 	OPEN_PASS(0)		{ public MiniGame getGame() { return new OpenPass(); } },		//Author: JerryEris
 	UP_AND_DOWN(0)		{ public MiniGame getGame() { return new UpAndDown(); } },		//Author: JerryEris
-	BOMB_ROULETTE(0)	{ public MiniGame getGame() { return new BombRoulette(); } },	//Author: StrangerCoug
 	DEUCES_WILD(0)		{ public MiniGame getGame() { return new DeucesWild(); } },		//Author: StrangerCoug
 	HILO_DICE(0)		{ public MiniGame getGame() { return new HiLoDice(); } },		//Author: StrangerCoug
 	SPLIT_WINNINGS(0)	{ public MiniGame getGame() { return new SplitWinnings(); } },	//Author: StrangerCoug
+	//Seasonal event is no longer a minigame lol
+	COMMISSIONER(0)		{ public MiniGame getGame() { return new TheCommissioner(); } },
 	
 	//Bonus Games - not in pool but earned through other means
 	SUPERCASH(0)		{ public MiniGame getGame() { return new Supercash(); } },
