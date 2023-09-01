2d57bebf715468292e356d18c5f567ed95cd1278
NotAJumbleOfNumbers <notajumbleofnumbers@gmail.com>
2023-09-01 07:42:40 -0500

    Update MemeCommand.java

--- src/tel/discord/rtab/commands/MemeCommand.java
+++ src/tel/discord/rtab/commands/MemeCommand.java
@@ -9,7 +9,7 @@ public class MemeCommand extends Command
 	{
 		this.name = "meme";
 		this.aliases = new String[]{"jo","realluckynumber","luckyletter","om","ub","starman","previous","instantbillion",
-				"peak","peep","pee","eep","noij","nioj","yvanehtnioj","ni","flex","bowserjackpot"};
+				"peak","peep","pee","eep","noij","nioj","yvanehtnioj","ni","flex","bowserjackpot","ho"};
 		this.help = "https://niceme.me";
 		this.hidden = true;
 		this.guildOnly = false;
