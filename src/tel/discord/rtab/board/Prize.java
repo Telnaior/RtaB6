b08a8ff62954b3b0ef9f4cfcc4c0056d4f85d050
Jerry <JerryRules@gmail.com>
2023-09-25 02:04:19 -0500

    Big big prize update

--- src/tel/discord/rtab/board/Prize.java
+++ src/tel/discord/rtab/board/Prize.java
@@ -15,51 +15,96 @@ public enum Prize {
 	S9TROPHY (  630000,"a replica of CouponBoy5's Lucky Season 9 trophy"),
 	S10TROPHY(   28000,"a replica of LA_Bud's Season 10 trophy"),
 	S11TROPHY(  110000,"a replica of vincentw's Season 11 trophy"),
+	S12TROPHY(   34000,"a replica of Lulu's Season 12 Revival trophy"),
+
+	//Unlucky Season 13 prizes
+	//Hi don't spoil these please
+	
+	MIRROR (-130000,"a broken mirror"),
+	SPADES (-130000,"an Ace of Spades"),
+	BLACKCAT (-130000,"a black cat"),
+	THIRTEEN (-130000,"a giant 13"),
+	LADDER (-130000,"a walked-under ladder"),
+	CALENDAR (-130000,"a calendar of Friday the 13ths"),
+	HORSESHOE (-130000,"an upside-down horseshoe"),
+	UMBRELLA (-130000,"an indoor umbrella"),
+	MACBETH (-130000,"a copy of Macbeth"),
+	SHAKER (-130000,"a spilled salt shaker"),
+	PENNY (-130000,"a tails-up penny"),
+	CHOPSTICKS (-130000,"a pair of pointed-up chopsticks in rice"),
+	ROCK (-130000,"a rock thrown into the wind"),
 	
 	//(Ir)Regular prizes
+	//Additional thanks to Kali, Lulu, supakorn_mhee, Mallow, KP, and many more who I regrettably neglected to write down
 	JOKERSIKE	(       1,"a fake Joker"), //RtaB meme
+	TRISCUIT	(       5,"THE TRI... scuit"), //Wow, not here? Try again!
+	EVERYTHING	(      15,"EVERYTHING... the video game"), //on Steam it's $15
+	DUCKS		(      22,"two little ducks"), //Bingo lingo
+	BULLSEYE	(      50,"a bullseye"), //Bullseye on a dartboard is worth 50
+	VOTE		(     100,"a vote"), //Blaseball
 	ZONK		(     123,"a Zonk"), //Let's Make a Deal
 	STCHARLES	(     140,"a trip to St. Charles Place"), //Monopoly
-	EWW			(     144,"something gross"), //Twelve dozen = a gross
+	EWW		(     144,"something gross"), //Twelve dozen = a gross
 	SNOOKER		(     147,"a snooker table"), //Maximum Break in Snooker
 	DARTBOARD	(     180,"a dartboard"), //3x Triple 20
 	SMALLTHINGS	(     182,"All the Small Things"), //Song by Blink-182
 	GOTOGO		(     200,"Advance to GO"), //Monopoly
+	KEROKERO	(     200,"Kerokero Cola"), //Super Mario RPG
 	VOWEL		(     250,"a vowel"), //Wheel of Fortune
 	BOWLING		(     300,"a bowling ball"), //Maximum score in Bowling
 	UNIFORCES	(     333,"the Triforce!!! Wait, no, that's just three Monoforces"), //Try again
 	FLOKATI		(     350,"a Flokati Rug"), //Press Your Luck joke prize
+	TUNINGFORK	(     440,"a tuning fork"), //Tuning forks are tuned to A = 440 Hz
+	NINEDARTER	(     501,"nine darts"), //The minimum number of darts needed to win at 501 is nine
+	TAU		(     628,"a hunk of tau"), //Maths, to avoid arguments my opinions on the one true circle constant will not be shown in this file
 	SLOTJACKPOT	(     777,"the JACKPOT... on a slot machine"), //7-7-7 is a traditionally high-value line on a slot machine
 	PEANUTS		(    1000,"a bucket of peanuts"), //Blaseball
 	ROCKEFELLER	(    1273,"a trip down Rockefeller Street"), //Eurovision song that became an osu! meme
 	BIGJON		(    1906,"the BigJon special"), //BigJon
+	SWIFTIE		(    1989,"a Taylor Swift album"), //
 	BOARDWALK	(    2000,"rent at the Boardwalk Hotel"), //Monopoly (Three cheap prizes, who'd have thought)
+	FLUTE		(    2000,"a flute (for drinking out of)"), //Blaseball
+	FIVEPS5S	(    2500,"five PS5's"), //LoadingReadyRun recurring skit
 	ATARI		(    2600,"a wood-paneled Atari system"), //The Atari VCS, also known as the Atari 2600
+	SAMPLER		(    4280,"the Mushroom Shop Sampler"), //Total value of Mario Party Mushroom Shop items
 	FREELIVES	(    9900,"99 free lives! ...in Mario"), //100 coins for an extra life
+	XXVWORDS	(   10000,"25 Words (or Less)"), //Game show
+	RTABHOMEGAME	(   13000,"Race to a Billion: The Home Game"), //Lots of game shows have home games
 	BUTTSPIE	(   18000,"a slice of Butterscotch Pie"), //Undertale
 	FEUD		(   20000,"Fast Money"), //Family Feud
 	BIFORCE		(   22222,"The Biforce"), //Still haven't found the Triforce, huh? Better keep looking!
 	HUNTER		(   22475,"Superportal codes"), //Spyro 2
-	DB1	 		(   22805,"a DesertBuck"), //Desert Bus for Hope
+	DB1	 	(   22805,"a DesertBuck"), //Desert Bus for Hope
 	GOVERNOR	(   26000,"the Governor's favourite"), //Deal or No Deal UK
 	WEIRDAL		(   27000,"Weird Al's accordion"), //Weird Al
 	ECONSTANT	(   27183,"some e"), //Maths
-	PI			(   31416,"a fresh pi"), //Maths
+	PI		(   31416,"a fresh pi"), //Maths
 	GOLDWII		(   36000,"a gold-plated Nintendo Wii"), //Once owned by Queen Elizabeth II
 	ROADSTER	(   50000,"a sleek 1999 roadster"), //Smash TV
+	CATCHPHRASE	(   50000,"a catchphrase"), //The game show Catchphrase
+	PLUSH		(   54000,"a Rosy Maple Moth plushie"), //Cute
 	NORMALCD	(   54321,"a Normal Countdown"), //Not a Final Countdown
+	THAILAUGH	(   55555,"Thai canned laughter"), //55555 is a way to express laughter in Thai
 	SPREAD		(   57300,"the Spread Shot"), //Konami, who created Contra, is 573 in Goroawase wordplay
 	QUESTION	(   64000,"the famous question"), //The $64,000 Question
 	SIXTEENBIT	(   65536,"a 16-bit trophy"), //2^16
 	DISCORD		(   70013,"a Discord lamp"), //My Little Pony
+	THEREMIN	(   73000,"a magical theremin"), //They are pretty cool musical instruments
 	POWERPELLET	(   76500,"a Power Pellet"), //Namco, who created Pacman, is 765 in Goroawase wordplay
 	SECONDS		(   86400,"a dollar a second"), //For a day
 	GRANDPIANO	(   88000,"a grand piano"), //Number of keys on a piano
+	MATHCLASS	(   99999,"a perfect math class"), //Cirno from the Touhous
 	HUNDREDG	(  100000,"a 100 Grand bar"), //Candy
 	PYRAMID		(  100000,"The Pyramid"), //The $100,000 Pyramid
+	MEMORY		(  100000,"a lovely memory"), //No reference, just neat
 	LARSON		(  110237,"The Michael Larson special"), //Winnings of the man who exploited Press Your Luck
 	VINTTWEET	(  140000,"a vintage Tweet"), //There used to be 140 characters in a Tweet
-	PIZZABOX	(  458000,"an infinitely regenerating pizza box"); //SCP-458
+	BATTLEAXE	(  200000,"a Dragon Battleaxe"), //Old School RuneScape
+	STRAWBERRY	(  202000,"a strawberry pie"), //202 strawberries in Celeste
+	HAMLET		(  269000,"a medium cash hamlet"), //Another LoadingReadyRun reference, from Dave's Spokesman
+	PIZZABOX	(  458000,"an infinitely regenerating pizza box"), //SCP-458
+	BLACKLOTUS	(  511000,"a Black Lotus"), //Magic: The Gathering card worth a lot
+	BUCKAMINUTE	(  525600,"a dollar a minute"); //in a year
     
     private final int prizeValue;
     private final String prizeName;
