dc9fdb7b0ce927ac84eb0a37d5978cf9491c96f2
Telnaior <Telnaior@users.noreply.github.com>
2023-10-02 14:22:40 +1100

    Show season ranks in !history
    +1 QoL

--- src/tel/discord/rtab/commands/HistoryCommand.java
+++ src/tel/discord/rtab/commands/HistoryCommand.java
@@ -9,8 +9,7 @@ import java.util.List;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.internal.utils.tuple.ImmutablePair;
-import net.dv8tion.jda.internal.utils.tuple.Pair;
+import net.dv8tion.jda.internal.utils.tuple.MutableTriple;
 import tel.discord.rtab.Achievement;
 
 public class HistoryCommand extends ParsingCommand
@@ -56,8 +55,9 @@ public class HistoryCommand extends ParsingCommand
 			String channelID = channel.getId(); //The command is flagged guild-only, so we know this won't be null
 			int season = 1;
 			int minRank = Integer.MAX_VALUE;
+			int maxRank = 0;
 			int wins = 0;
-			List<Pair<Integer,Long>> cashFigures = new LinkedList<>();
+			List<MutableTriple<Integer,Integer,Long>> cashFigures = new LinkedList<>();
 			//We're going to keep reading history files as long as they're there
 			while(Files.exists(Paths.get("scores","history"+channelID+"s"+season+".csv")))
 			{
@@ -73,9 +73,11 @@ public class HistoryCommand extends ParsingCommand
 				if(index >= 0 && index < list.size())
 				{
 					String[] record = list.get(index).split("#");
-					cashFigures.add(ImmutablePair.of(season,Long.parseLong(record[2])));
+					cashFigures.add(MutableTriple.of(season,(index+1),Long.parseLong(record[2])));
 					if(index < minRank)
 						minRank = index;
+					if(index > maxRank)
+						maxRank = index+1;
 					if(index == 0)
 						wins++;
 					if(uID != null)
@@ -92,6 +94,7 @@ public class HistoryCommand extends ParsingCommand
 			//Loop through each season and calculate stats
 			StringBuilder seasonList = new StringBuilder();
 			int moneyWidth = minRank == 0 ? 17 : 13;
+			int rankWidth = maxRank == 1 ? 1 : (int)Math.log10(maxRank-1)+1;
 			//season stops at the first one that doesn't exist, so we subtract 1
 			//and if there's no history data at all (so season 1 is the first that doesn't exist) we have a failsave to avoid div-by-0
 			int seasonWidth = season == 1 ? 1 : (int)Math.log10(season-1)+1;
@@ -103,7 +106,7 @@ public class HistoryCommand extends ParsingCommand
 			int veteranSeasons = 0;
 			int regularSeasons = 0;
 			int grinderSeasons = 0;
-			for(Pair<Integer,Long> nextSeason : cashFigures)
+			for(MutableTriple<Integer,Integer,Long> nextSeason : cashFigures)
 			{
 				seasonsPlayed ++;
 				thisSeason = nextSeason.getRight();
@@ -111,7 +114,8 @@ public class HistoryCommand extends ParsingCommand
 				maingameCash += Math.min(1_000_000_000, thisSeason);
 				if(thisSeason > bestResult)
 					bestResult = thisSeason;
-				seasonList.append(String.format("Season %1$"+seasonWidth+"d: $%2$,"+moneyWidth+"d\n",nextSeason.getLeft(),thisSeason));
+				seasonList.append(String.format("Season %"+seasonWidth+"d: $%,"+moneyWidth+"d"
+						+ " - #%"+rankWidth+"d\n",nextSeason.getLeft(), thisSeason, nextSeason.getMiddle()));
 				//Achievement Check
 				if(thisSeason >= 100_000_000)
 				{
