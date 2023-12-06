3251349d2ff2ce470e0a244d1a07329ee70dea7a
Jerry <JerryRules@gmail.com>
2023-12-06 01:07:10 -0600

    Update SomethingForEveryone.java

--- src/tel/discord/rtab/events/SomethingForEveryone.java
+++ src/tel/discord/rtab/events/SomethingForEveryone.java
@@ -27,10 +27,57 @@ public class SomethingForEveryone implements EventSpace
 		game.channel.sendMessage("It's **Something for Everyone**!").queue();
 
 		for(Player nextPlayer : game.players)
+		{
 			if(nextPlayer.status == PlayerStatus.ALIVE)
-				//nextPlayer.awardHiddenCommand();
+			{
+				switch (100 * RtaBMath.random())
+				{
+						
+				//determine random chance here
+					case 0 ->
+					{
+						//hidden command
+					}
+					case 1 ->
+					{
+						//peek
+					}
+					case 2 ->
+					{
+						//one buck behind
+					}
+					case 3 to 5 ->
+					{
+						//annuity
+					}
+					case 6 to 9 ->
+					{
+						//streak
+					}
+					case else ->
+					{
+						switch (100 * RtaBMath.random())
+						{
+							case 0 to 39 ->
+							{
+								//cash
+							}
+							case 40 to 75 ->
+							{
+								//boost
+							}
+							case 76 to 99 ->
+							{
+								//minigame
+							}
+						}	
+					}
+				}
+			}
+		}
+	//nextPlayer.awardHiddenCommand();
         //int cashGiven = game.applyBaseMultiplier(50_000 + (int)(50_001 * RtaBMath.random())) * game.players.size() / game.playersAlive;
-		
+	//nextPlayer.addMoney(cashGiven, MoneyMultipliersToUse.BOOSTER_ONLY);
 	}
 
 }
