ecf55b4dea77957d8770c815ac97c4cccfba7519
Telnaior <Telnaior@users.noreply.github.com>
2024-02-18 13:52:53 +1100

    Deal or No Deal Revamp
    base game gets a bit of a buff (including the previous enhancement baked in), and a new enhancement added based on player feedback.

--- src/tel/discord/rtab/games/DealOrNoDeal.java
+++ src/tel/discord/rtab/games/DealOrNoDeal.java
@@ -14,14 +14,13 @@ public class DealOrNoDeal extends MiniGameWrapper
 	static final String SHORT_NAME = "Deal";
 	static final boolean BONUS = false;
 	List<Integer> VALUE_LIST = Arrays.asList(1,10,50,100,250,500,750,1_000,2_500,5_000,7_500, //Blues
-			10_000,30_000,50_000,100_000,200_000,350_000,500_000,750_000,1_000_000,2_000_000,5_000_000); //Reds
-	List<Integer> VALUE_LIST_ENHANCED = Arrays.asList(1,10,50,100,250,500,750,1_000,2_500,5_000,7_500, //Blues
-			10_000,30_000,50_000,100_000,200_000,500_000,750_000,1_000_000,2_000_000,3_500_000,5_000_000); //Reds
+			10_000,30_000,50_000,100_000,250_000,500_000,750_000,1_000_000,2_000_000,3_500_000,5_000_000); //Reds
 	LinkedList<Integer> values = new LinkedList<>();
 	int offer;
 	int prizeWon;
 	int casesLeft;
 	int moneyLength;
+	boolean counterAvailable;
 	boolean accept; //Accepting the Offer
 
 	@Override
@@ -30,9 +29,8 @@ public class DealOrNoDeal extends MiniGameWrapper
 		casesLeft = VALUE_LIST.size();
 		offer = 0;
 		accept = false;
+		counterAvailable = enhanced;
 		//Enhanced players get the buffed board
-		if(enhanced)
-			VALUE_LIST = VALUE_LIST_ENHANCED;
 		//Multiply each value, EXCEPT the $1, by the base multiplier
 		for(int i = 1; i < VALUE_LIST.size(); i++)
 		{
@@ -47,14 +45,16 @@ public class DealOrNoDeal extends MiniGameWrapper
 		//Give instructions
 		LinkedList<String> output = new LinkedList<>();
 		output.add("In Deal or No Deal, there are 22 boxes, "
-				+ String.format("each holding an amount of money from $1 to $%,d.",applyBaseMultiplier(5_000_000)));
+				+ String.format("each holding an amount of money from $1 to $%,d.",applyBaseMultiplier(VALUE_LIST.get(casesLeft-1))));
 		output.add("One of these boxes is 'yours', and if you refuse all the offers you win the contents of that box.");
 		output.add("We open the other boxes one by one to find out which values *aren't* in your own box.");
-		output.add("The first offer comes after five boxes are opened, after which offers are received every three boxes.");
+		output.add("The first offer comes when there are 17 boxes left, then at 14, 11, 8, 5, 3, and 2 boxes left.");
 		output.add("If you take an offer at any time, you win that amount instead of the contents of the final box.");
 		if(enhanced)
-			output.add(String.format("ENHANCE BONUS: The $%,d box has been replaced with $%,d.",
-					applyBaseMultiplier(350_000),applyBaseMultiplier(3_500_000)));
+		{
+			output.add("ENHANCE BONUS: You can make a counteroffer once during the game "
+					+ "by typing 'COUNTER' followed by the amount you would like to offer. The bank can accept or refuse this.");
+		}
 		output.add("Best of luck, let's start the game...");
 		sendSkippableMessages(output);
 		output.clear();
@@ -82,41 +82,112 @@ public class DealOrNoDeal extends MiniGameWrapper
 		LinkedList<String> output = new LinkedList<>();
 		String choice = pick.toUpperCase();
 		choice = choice.replaceAll("\\s","");
-		if(choice.equals("REFUSE") || choice.equals("NODEAL") || choice.equals("ND"))
+		if(choice.startsWith("COUNTER"))
 		{
-			output.add("NO DEAL!");
-			if(casesLeft == 2)
+			if(counterAvailable)
 			{
-				output.add("Your box contains...");
-				prizeWon = values.pollLast();
-				output.add(String.format("$%,d!",prizeWon));
-				accept = true;
+				//get the money amount
+				String[] tokens = pick.split("\\s");
+				int counterAmount = parseMoney(tokens[1]);
+				if(counterAmount > offer)
+				{
+					output.add(String.format("Counteroffering $%,d...", counterAmount));
+					counterAvailable = false;
+					output.add("The Banker says...");
+					//Roll accept chance
+					int average = 0;
+					for(int i : values)
+						average += i;
+					average /= casesLeft;
+					double gap = average - offer;
+					if(Math.random() < ((counterAmount-offer)/gap))
+					{
+						output.add("NO DEAL!");
+						sendMessages(output);
+						playNextRound();
+					}
+					else
+					{
+						output.add("DEAL!");
+						accept = true;
+						prizeWon = counterAmount;
+						sendMessages(output);
+						runProveout();
+					}
+				}
 			}
 			else
-			{
-				output.add("Opening three boxes...");
-				for(int i=0; i<3; i++)
-					output.add(openBox());
-				output.add("...");
-				output.add(generateOffer());
-				output.add("Deal or No Deal?");
-				output.add(generateBoard());
-			}
+				sendMessage("You don't have a counteroffer to use.");
+		}
+		else if(choice.equals("REFUSE") || choice.equals("NODEAL") || choice.equals("ND"))
+		{
+			output.add("NO DEAL!");
 			sendMessages(output);
+			playNextRound();
 		}
 		else if(choice.equals("ACCEPT") || choice.equals("DEAL") || choice.equals("D"))
 		{
 			accept = true;
 			prizeWon = offer;
 			output.add("It's a DONE DEAL!");
-			output.add("Now for the proveout... (you can !skip this)");
 			sendMessages(output);
-			sendSkippableMessages(runProveout());
+			runProveout();
 		}
 		if(accept)
 			awardMoneyWon(prizeWon);
 		else
 			getInput();
+	}
+	
+	private void playNextRound()
+	{
+		LinkedList<String> output = new LinkedList<>();
+		if(casesLeft == 2)
+		{
+			output.add("Your box contains...");
+			prizeWon = values.pollLast();
+			output.add(String.format("$%,d!",prizeWon));
+			accept = true;
+		}
+		else
+		{
+			int casesToOpen;
+			if(casesLeft == 5)
+			{
+				output.add("Opening two boxes...");
+				casesToOpen = 2;
+			}
+			else
+			{
+				output.add("Opening three boxes...");
+				casesToOpen = 3;
+			}
+			for(int i=0; i<casesToOpen; i++)
+				output.add(openBox());
+			output.add("...");
+			output.add(generateOffer());
+			output.add("Deal or No Deal?");
+			output.add(generateBoard());
+		}
+		sendMessages(output);
+	}
+	
+	private void runProveout()
+	{
+		LinkedList<String> output = new LinkedList<>();
+		output.add("Now for the proveout... (you can !skip this)");
+		while(casesLeft > 2)
+		{
+			StringBuilder boxesOpened = new StringBuilder();
+			for(int i=0; i<3; i++)
+				boxesOpened.append(openBox()).append(" ");
+			output.add(boxesOpened.toString());
+			generateOffer();
+			output.add(generateBoard());
+		}
+		output.add("Your box contained...");
+		output.add(String.format("$%,d!",values.pollLast()));
+		sendSkippableMessages(output);
 	}
 
 	private String generateOffer()
@@ -184,23 +255,6 @@ public class DealOrNoDeal extends MiniGameWrapper
 		output.append("```");
 		return output.toString();
 	}
-	
-	private LinkedList<String> runProveout()
-	{
-		LinkedList<String> output = new LinkedList<>();
-		while(casesLeft > 2)
-		{
-			StringBuilder boxesOpened = new StringBuilder();
-			for(int i=0; i<3; i++)
-				boxesOpened.append(openBox()).append(" ");
-			output.add(boxesOpened.toString());
-			generateOffer();
-			output.add(generateBoard());
-		}
-		output.add("Your box contained...");
-		output.add(String.format("$%,d!",values.pollLast()));
-		return output;
-	}
 
 	@Override
 	String getBotPick() {
@@ -221,5 +275,5 @@ public class DealOrNoDeal extends MiniGameWrapper
 	@Override public String getName() { return NAME; }
 	@Override public String getShortName() { return SHORT_NAME; }
 	@Override public boolean isBonus() { return BONUS; }
-	@Override public String getEnhanceText() { return "The box containing 7% of the jackpot will have its value multiplied by 10 (to 70%)."; }
+	@Override public String getEnhanceText() { return "You can make one counteroffer at any point in the game."; }
 }
