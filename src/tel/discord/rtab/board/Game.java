6a06b21b5da801f9a983375129d70fb8cd97d786
Telnaior <Telnaior@users.noreply.github.com>
2024-11-07 16:13:10 +1100

    Update minigame pool
    (for real this time)

--- src/tel/discord/rtab/board/Game.java
+++ src/tel/discord/rtab/board/Game.java
@@ -17,7 +17,7 @@ public enum Game implements WeightedSpace
 	DEAL_OR_NO_DEAL(2)	{ public MiniGame getGame() { return new DealOrNoDeal(); } },	//Author: Telna
 	FTROTS(2)			{ public MiniGame getGame() { return new FTROTS(); } },			//Author: Telna
 	TRIPLE_PLAY(2)		{ public MiniGame getGame() { return new TriplePlay(); } },		//Author: Telna
-	MINEFIELD_MULTI(0)	{ public MiniGame getGame() { return new MinefieldMulti(); } },	//Author: Amy
+	MINEFIELD_MULTI(2)	{ public MiniGame getGame() { return new MinefieldMulti(); } },	//Author: Amy
 	CLOSE_SHAVE(2)		{ public MiniGame getGame() { return new CloseShave(); } },		//Author: JerryEris
 	STARDUST(2)			{ public MiniGame getGame() { return new Stardust(); } },		//Author: NicoHolic777
 	FIFTY_YARD_DASH(2)	{ public MiniGame getGame() { return new FiftyYardDash(); } },	//Author: StrangerCoug
@@ -40,7 +40,7 @@ public enum Game implements WeightedSpace
 	//DOUBLE_ZEROES(0)	{ public MiniGame getGame() { return new DoubleZeroes(); } },	//Author: JerryEris (disabled until fixed)
 	OPEN_PASS(0)		{ public MiniGame getGame() { return new OpenPass(); } },		//Author: JerryEris
 	UP_AND_DOWN(0)		{ public MiniGame getGame() { return new UpAndDown(); } },		//Author: JerryEris
-	BOMB_ROULETTE(2)	{ public MiniGame getGame() { return new BombRoulette(); } },	//Author: StrangerCoug
+	BOMB_ROULETTE(0)	{ public MiniGame getGame() { return new BombRoulette(); } },	//Author: StrangerCoug
 	PUNCH_A_BUNCH(0)	{ public MiniGame getGame() { return new PunchABunch(); } },    //Author: StrangerCoug
 	SPLIT_WINNINGS(0)	{ public MiniGame getGame() { return new SplitWinnings(); } },	//Author: StrangerCoug
 	
